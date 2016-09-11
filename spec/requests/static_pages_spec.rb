e01f9d01e6445dd55c024a33b5f44de61c321c92
Yassine Lachguar <yassau@gmail.com>
2016-09-11 14:18:50 +0200

    add about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
-describe "Static pages" do
-  describe "Home page" do
+feature "Static pages" do
+  feature "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have content('Sample App')
+      page.should have_content('Sample App')
+    end 
+  end
+end
+
+feature "Static pages" do
+  feature "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
     end 
   end
 end
