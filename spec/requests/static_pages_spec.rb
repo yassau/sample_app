167eedd1b2941e73b33712de88c8739e8ae20e9d
Yassine Lachguar <yassau@gmail.com>
2016-09-11 16:12:14 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,6 +13,11 @@ feature "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+    it "should have the right title" do 
+      visit '/static_pages/about' 
+      page.should have_selector('title',:text => "Ruby on Rails Tutorial Sample App | About Us")
     end 
   end
 end
+
